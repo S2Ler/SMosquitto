f362bd84b8e15d93fb5d7b6a1bb922d9a7750e32
Alexander Belyavskiy <diejmon@gmail.com>
2019-01-08 08:43:47 +0300

    Add init method

--- Sources/SMosquitto/SMosquitto.swift
+++ Sources/SMosquitto/SMosquitto.swift
@@ -1,6 +1,7 @@
 import cmosquitto
 
 public class SMosquitto {
+  private let handle: OpaquePointer
   public static func initialize() {
     mosquitto_lib_init();
   }
@@ -11,5 +12,14 @@ public class SMosquitto {
 
   public static func version() -> SMosquittoVersion {
     return SMosquittoVersion()
+  }
+
+  public init(id: String? = nil, cleanSession: Bool = true) {
+    if let id = id {
+      self.handle = mosquitto_new(id, cleanSession, nil)
+    }
+    else {
+      self.handle = mosquitto_new(nil, cleanSession, nil)
+    }
   }
 }

f4da9aa0c5b85e8f1ef2cf7c759f222da4c7044a
Alexander Belyavskiy <diejmon@gmail.com>
2019-01-09 09:23:02 +0300

    Update linux tests

--- Tests/SMosquittoTests/XCTestManifests.swift
+++ Tests/SMosquittoTests/XCTestManifests.swift
@@ -9,10 +9,28 @@ extension SMosquittoErrorTests {
     ]
 }
 
+extension SMosquittoLogLevelTests {
+    static let __allTests = [
+        ("testCreatable", testCreatable),
+        ("testDescription", testDescription),
+        ("testInvalidLogLevel", testInvalidLogLevel),
+    ]
+}
+
+extension SMosquittoVersionTests {
+    static let __allTests = [
+        ("testCodable", testCodable),
+        ("testHashable", testHashable),
+        ("testInSupportedVersionsRange", testInSupportedVersionsRange),
+    ]
+}
+
 #if !os(macOS)
 public func __allTests() -> [XCTestCaseEntry] {
     return [
         testCase(SMosquittoErrorTests.__allTests),
+        testCase(SMosquittoLogLevelTests.__allTests),
+        testCase(SMosquittoVersionTests.__allTests),
     ]
 }
 #endif
